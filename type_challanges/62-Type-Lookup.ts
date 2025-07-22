c1e854af3a0ffcba8cb444785f5f5f6b17b46d57
three <Threeee33@outlook.com>
2025-07-22 18:07:36 +0800

    +2

---
+++ type_challanges/62-Type-Lookup.ts
@@ -0,0 +1,66 @@
+/*
+  62 - Type Lookup
+  -------
+  by Anthony Fu (@antfu) #medium #union #map
+
+  ### Question
+
+  Sometimes, you may want to look up a type in a union by its attributes.
+
+  In this challenge, we would like to get the corresponding type by searching for the common `type` field in the union `Cat | Dog`. In other words, we will expect to get `Dog` for `LookUp<Dog | Cat, 'dog'>` and `Cat` for `LookUp<Dog | Cat, 'cat'>` in the following example.
+
+    有时候，你可能希望通过联合类型中的某个属性来查找对应的类型。
+
+    在本题中，我们希望通过联合类型 Cat | Dog 中共有的 type 字段来查找对应的类型。
+
+    换句话说，对于下面的例子，我们期望 LookUp<Dog | Cat, 'dog'> 得到 Dog，LookUp<Dog | Cat, 'cat'> 得到 Cat。
+
+  ```ts
+  interface Cat {
+    type: 'cat'
+    breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+  }
+
+  interface Dog {
+    type: 'dog'
+    breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+    color: 'brown' | 'white' | 'black'
+  }
+
+  type MyDogType = LookUp<Cat | Dog, 'dog'> // expected to be `Dog`
+  ```
+
+  > View on GitHub: https://tsch.js.org/62
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type LookUp<U, T> = U extends {type : T} ? U : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
+
+type cases = [
+  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/62/answer
+  > View solutions: https://tsch.js.org/62/solutions
+  > More Challenges: https://tsch.js.org
+*/
